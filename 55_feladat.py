cd2cd4eff42bfae085020ef5c2b4327fb6fd33df
bkata64 <64857455+bkata64@users.noreply.github.com>
2022-01-29 21:33:30 +0100

    GUI solution

--- 55_feladat.py
+++ 55_feladat.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 class Helyiseg:
     def __init__(self, szelesseg, hosszusag, magassag):
         self.szelesseg = szelesseg
@@ -14,21 +16,26 @@ class Helyiseg:
         return self.falterulet() + self.alapterulet()
 
 
-def beker():
-    helyisegek_szama = int(
-        input("Hány helyiségben szeretne kövezni/festeni? "))
-    hely = []
-    for i in range(helyisegek_szama):
-        print(f"{i + 1}. helyiség adatai:")
-        sz = float(input("\tHelyiség szélesség: "))
-        h = float(input("\tHelyiség hosszúság: "))
-        m = float(input("\tHelyiség magasság: "))
-        helyiseg = Helyiseg(sz, h, m)
-        hely.append(helyiseg)
-    return hely
+def rogzit():    
+    sz = float(ent_sz.get())
+    h = float(ent_h.get())
+    m = float(ent_m.get())    
+    helyiseg = Helyiseg(sz, h, m)
+    helyisegek.append(helyiseg)
+    megjelenit()    
+
+def modosit():        
+    sz = float(ent_sz.get())
+    h = float(ent_h.get())
+    m = float(ent_m.get()) 
+    helyisegek[sorszam].szelesseg = sz   
+    helyisegek[sorszam].hosszusag = h
+    helyisegek[sorszam].magassag = m
+    megjelenit()
 
 def beolvas():
-    lista = []
+    global sorszam
+    #helyisegek = []
     with open("hely.txt", "r") as file:
         for sor in file:
             adatok = sor.strip().split()
@@ -36,36 +43,88 @@ def beolvas():
             h = float(adatok[1])
             m = float(adatok[2])
             helyiseg = Helyiseg(sz, h, m)
-            lista.append(helyiseg)
-    return lista
+            helyisegek.append(helyiseg)
+    sorszam = len(helyisegek) - 1
+    megjelenit()
 
-def kiertekel(helyisegek):
-    print("\nA szükséges alapanyagok mennyisége:")
-    mennyiseg_m2 = 0.13
-    ossz_at = 0
-    ossz_ff = 0
-    for i, hely_ in enumerate(helyisegek):
-        print(f"A(z) {i + 1}. helyiséghez szükséges:")
-        at = hely_.alapterulet()
-        ossz_at += at
-        print(f"\tPadlóburkoló: {at} m2")
-        ff = hely_.festett_felulet()
-        ossz_ff += ff
-        print(f"\tFesték: {round(ff * mennyiseg_m2)} l")
+def kiiras():
+    pass
 
-    print(f"\nÖsszes szükséges padlóburkoló: {ossz_at} m2")
-    print(f"Összes szükséges festékmennyiség: {round(ossz_ff * mennyiseg_m2)} l")
+def megjelenit():
+    ent_sz.delete(0,5)
+    ent_h.delete(0,5) 
+    ent_m.delete(0,5)
+    if sorszam < len(helyisegek):
+        ent_sz.insert(0, helyisegek[sorszam].szelesseg)
+        ent_h.insert(0, helyisegek[sorszam].hosszusag)
+        ent_m.insert(0, helyisegek[sorszam].magassag)
+        lbl_b["text"] = f"Padlóburkoló: {round(helyisegek[sorszam].alapterulet())}m2"
+        lbl_f["text"] = f"Festék: {round(helyisegek[sorszam].festett_felulet() * mennyiseg_m2)} l"
+        btn_rogzit["text"] = "Módosít"
+        btn_rogzit["command"] = modosit
+    else:
+        btn_rogzit["text"] = "Rögzít"
+        btn_rogzit["command"] = rogzit
+        lbl_b["text"] = "Padlóburkoló:"
+        lbl_f["text"] = "Festék:"
+    lbl_record["text"] = f"{sorszam + 1}/{len(helyisegek)}"    
 
-def main():
-    print("Helyiségek festése, kövezése")
-    valasz = input("Az adatokat állományból olvassuk be? (i / n): ")
-    helyisegek = []
-    if valasz == "i":
-        helyisegek = beolvas()
-    elif valasz == "n":
-        helyisegek = beker()
-    kiertekel(helyisegek)
+def vissza():
+    global sorszam
+    if sorszam > 0:
+        sorszam -= 1
+        megjelenit()
 
-main()
+def elore():
+    global sorszam
+    if sorszam < len(helyisegek):
+        sorszam += 1
+        megjelenit()
+
+
+
+helyisegek = []
+sorszam = -1
+mennyiseg_m2 = 0.13
+window = tk.Tk()
+window.title("Helyiségek kalkulátor")
+lblCim = tk.Label(text="Helyiségek festése, kövezése", font=("Arial", 20))
+lblCim.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+btnBeolv = tk.Button(text="Adatok beolvasása", command=beolvas)
+btnBeolv.grid(row=1, column=0)
+btnKiir = tk.Button(text="Adatok kiírása", command=kiiras)
+btnKiir.grid(row=1, column=1)
+
+lbl_sz = tk.Label(text="Szélesség")
+ent_sz = tk.Entry(width=15)
+lbl_b = tk.Label(text="Padlóburkoló:", font=("Arial",12))
+lbl_sz.grid(row=2, column=0)
+ent_sz.grid(row=2, column=1)
+lbl_b.grid(row=2, column=2)
+lbl_h = tk.Label(text="Hosszúság")
+ent_h = tk.Entry(width=15)
+lbl_f = tk.Label(text="Festék:", font=("Arial",12))
+lbl_h.grid(row=3, column=0)
+ent_h.grid(row=3, column=1)
+lbl_f.grid(row=3, column=2)
+lbl_m = tk.Label(text="Magasság")
+ent_m = tk.Entry(width=15)
+lbl_m.grid(row=4, column=0)
+ent_m.grid(row=4, column=1)
+
+
+frm_buttons = tk.Frame()
+frm_buttons.grid(row=5, column=0, columnspan=3)
+btn_vissza = tk.Button(master=frm_buttons, text="<", command=vissza)
+btn_vissza.grid(row=0, column=0)
+lbl_record = tk.Label(master=frm_buttons, text=f"{sorszam}/{len(helyisegek)}")
+lbl_record.grid(row=0, column=1)
+btn_elore = tk.Button(master=frm_buttons, text=">", command=elore)
+btn_elore.grid(row=0, column=2)
+btn_rogzit = tk.Button(master=frm_buttons, text="Rögzít", command=rogzit)
+btn_rogzit.grid(row=0, column=3)
+
+window.mainloop()
+
 
 
